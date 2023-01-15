ae743982262c03895b4ee8c0b1bdbec1c8b369ba
shafik720 <shafikrasel5@gmail.com>
2023-01-16 02:05:14 +0600

    added new data

---
+++ all-projects.js
@@ -0,0 +1,51 @@
+const allProjects = [
+    {
+        id : 1, 
+        github : 'https://github.com/shafik720/Photographer-Website-Portfolio---E-commerce--with-React',
+        liveSite : 'https://photographer-portfolio-shafik.netlify.app/',
+        projectName : `Photographer's Website (Portfolio + E-commerce)`,
+        desc : `This is a website where a photographer portfolio is displayed alongside with e-commerce functionality. <br />
+        This site is fully responsive. It's made with React Js, Firebase Authentication, React Bootstrap. For storing user cart information i have used Local Storage.`,
+        technology : ['Html','Css','Javascript','React','Firebase','Local Storage','React Bootstrap','React Router',]
+    },
+    {
+        id : 2, 
+        github : 'https://github.com/shafik720/Tetris-Game-with-Vanilla-Javascript-Html-Css',
+        liveSite : 'https://tetris-russell.netlify.app/',
+        projectName : `Tetris Game`,
+        desc : `I made this famous childhood tetris game with Vanilla Javascript, Html, Css`,
+        technology : ['Html','Css','Javascript']
+    },
+    {
+        id : 3, 
+        github : 'https://github.com/shafik720/Card-Memory-Game-with-Javascript-Html-Css',
+        liveSite : 'https://card-memory-game-shafiq.netlify.app/',
+        projectName : `Card Memory Game `,
+        desc : `This is a Card Memory Game where user have to memorize pictures and guess them correctly. I made this game with Vanilla Javascript, Html, Css only.`,
+        technology : ['Html','Css','Javascript']
+    },
+    {
+        id : 4, 
+        github : 'https://github.com/shafik720/Project---Math-Game',
+        liveSite : 'https://math-game-by-shafiq.netlify.app/',
+        projectName : `Math Game`,
+        desc : `User will play this game by selecting the correct box with the correct result of multiplying 2 numbers showing at the top of the screen. There will be 4 boxes, Only one of them will have right answers only. This game is made with Vanilla Javascript, Html, Css`,
+        technology : ['Html','Css','Javascript']
+    },
+    {
+        id : 5, 
+        github : 'https://github.com/shafik720/Language-translator-with-Javascript-Html-Css',
+        liveSite : 'https://translator-by-shafiq.netlify.app/',
+        projectName : `Language Translator App`,
+        desc : `This site will translate any language to any selected language. For this i have used language api. This site is made with Vanilla Javascript, Html, Css`,
+        technology : ['Html','Css','Javascript', 'API']
+    },
+    {
+        id : 6, 
+        github : 'https://github.com/shafik720/currency-converter',
+        liveSite : 'https://currency-converter-by-shafiq.netlify.app/',
+        projectName : `Language Translator App`,
+        desc : `This site will convert from any currency to any currency. The currency rate is always updated via api`,
+        technology : ['Html','Css','Javascript', 'API']
+    },
+]
