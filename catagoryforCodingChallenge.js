ee04cc0885ad2234e79bd494ef317b56024c96ee
shafik720 <shafikrasel5@gmail.com>
2023-01-18 22:27:11 +0600

    database data error solved

---
+++ catagoryforCodingChallenge.js
@@ -0,0 +1,99 @@
+[
+    {"deta": [1,"Manufacturing"]},
+    {"deta": [19,"\u00A0 \u00A0 \u00A0 \u00A0 Construction materials"]},
+    {"deta": [18,"\u00A0 \u00A0 \u00A0 \u00A0 Electronics and Optics"]},
+    {"deta": [6,"\u00A0 \u00A0 \u00A0 \u00A0 Food and Beverage"]},
+    {"deta": [342,"\u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 Bakery & confectionery products"]},
+    {"deta": [43,"\u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 Beverages"]},
+    {"deta": [42,"\u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 Fish & fish products "]},
+    {"deta": [40,"\u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 Meat & meat products"]},
+    {"deta": [39,"\u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 Milk & dairy products"]},
+    {"deta": [437,"\u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 Other"]},
+    {"deta": [378,"\u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 Sweets & snack food"]},
+    {"deta": [13,"\u00A0 \u00A0 \u00A0 \u00A0 Furniture "]},
+    {"deta": [389,"\u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 Bathroom/sauna "]},
+    {"deta": [385,"\u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 Bedroom"]},
+    {"deta": [390,"\u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 Children's room "]},
+    {"deta": [98,"\u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 Kitchen "]},
+    {"deta": [101,"\u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 Living room  "]},
+    {"deta": [392,"\u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 Office"]},
+    {"deta": [394,"\u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 Other (Furniture)"]},
+    {"deta": [341,"\u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 Outdoor "]},
+    {"deta": [99,"\u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 Project furniture"]},
+    {"deta": [12,"\u00A0 \u00A0 \u00A0 \u00A0 Machinery"]},
+    {"deta": [94,"\u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 Machinery components"]},
+    {"deta": [91,"\u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 Machinery equipment/tools"]},
+    {"deta": [97,"\u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 Maritime"]},
+    {"deta": [271,"\u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 Aluminium and steel workboats"]},
+    {"deta": [269,"\\u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 Boat/Yacht building"]},
+    {"deta": [230,"\u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 Ship repair and conversion"]},
+    {"deta": [93,"u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 Metal structures"]},
+    {"deta": [508,"\u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 Other"]},
+    {"deta": [227,"\\u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 Repair and maintenance service"]},
+    {"deta": [11,"\u00A0 \u00A0 \u00A0 \u00A0 Metalworking"]},
+    {"deta": [67,"\u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 Construction of metal structures"]},
+    {"deta": [263,"u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 Houses and buildings"]},
+    {"deta": [267,"\u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 Metal products"]},
+    {"deta": [542,"\u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 Metal works"]},
+    {"deta": [75,"\u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 CNC-machining"]},
+    {"deta": [62,"\u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 Forgings, Fasteners"]},
+    {"deta": [69,"\u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 Gas, Plasma, Laser cutting"]},
+    {"deta": [66,"\u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 MIG, TIG, Aluminum welding"]},
+    {"deta": [9,"\u00A0 \u00A0 \u00A0 \u00A0 Plastic and Rubber"]},
+    {"deta": [54,"\u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 Packaging"]},
+    {"deta": [556,"\u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 Plastic goods"]},
+    {"deta": [559,"\u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 Plastic processing technology"]},
+    {"deta": [55,"\u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 Blowing"]},
+    {"deta": [57,"\u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 Moulding"]},
+    {"deta": [53,"\u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 Plastics welding and processing"]},
+    {"deta": [560,"\u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 Plastic profiles"]},
+    {"deta": [5,"\u00A0 \u00A0 \u00A0 \u00A0 Printing "]},
+    {"deta": [148,"\u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 Advertising"]},
+    {"deta": [150,"\u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 Book/Periodicals printing"]},
+    {"deta": [145,"\u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 Labelling and packaging printing"]},
+    {"deta": [7,"\u00A0 \u00A0 \u00A0 \u00A0 Textile and Clothing"]},
+    {"deta": [44,"\u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 Clothing"]},
+    {"deta": [45,"\u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 Textile"]},
+    {"deta": [8,"\u00A0 \u00A0 \u00A0 \u00A0 Wood"]},
+    {"deta": [337,"\u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 Other (Wood)"]},
+    {"deta": [51,"\u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 Wooden building materials"]},
+    {"deta": [47,"\u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 Wooden houses"]},
+    {"deta": [3,"Other"]},
+    
+    {"deta": [37,"\u00A0 \u00A0 \u00A0 \u00A0 Creative industries"]},
+    
+    {"deta": [29,"\u00A0 \u00A0 \u00A0 \u00A0 Energy technology"]},
+    
+    {"deta": [33,"\u00A0 \u00A0 \u00A0 \u00A0 Environment"]},
+    
+    {"deta": [2,"Service"]},
+    
+    {"deta": [25,"\u00A0 \u00A0 \u00A0 \u00A0 Business services"]},
+    
+    {"deta": [35,"\u00A0 \u00A0 \u00A0 \u00A0 Engineering"]},
+    
+    {"deta": [28,"u00A0 \u00A0 \u00A0 \u00A0 Information Technology and Telecommunications"]},
+    
+    {"deta": [581,"\u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 Data processing, Web portals, E-marketing"]},
+    
+    {"deta": [576,"\u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 Programming, Consultancy"]},
+    
+    {"deta": [121,"\u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 Software, Hardware"]},
+    
+    {"deta": [122,"\u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 Telecommunications"]},
+    
+    {"deta": [22,"\u00A0 \u00A0 \u00A0 \u00A0 Tourism"]},
+    
+    {"deta": [141,"\u00A0 \u00A0 \u00A0 \u00A0 Translation services"]},
+    
+    {"deta": [21,"\u00A0 \u00A0 \u00A0 \u00A0 Transport and Logistics"]},
+    
+    {"deta": [111,"\u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 Air"]},
+    
+    {"deta": [114,"\u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 Rail"]},
+    
+    {"deta": [112,"\u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 Road"]},
+    
+    {"deta": [113,"\u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 \u00A0 Water"]}
+  
+  ]
