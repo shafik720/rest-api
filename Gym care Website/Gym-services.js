320b56d17ec6502726a457c2e829a417c9752947
shafik720 <shafikrasel5@gmail.com>
2023-05-03 20:22:11 +0600

    gym website data

---
+++ Gym care Website/Gym-services.js
@@ -0,0 +1,46 @@
+
+
+const services = [
+    {
+        serviceId : 1 ,
+        plan : 'Basic',
+        serviceName : 'Fitness For Beginners',
+        short_desc : 'Make a list of your achievements toward your long-term goal and remind.',
+        basic_plan : {
+            shortDesc : 'Positive pleasure-oriented goals are much more powerful.',
+            price : 150 , 
+            duration : 3,
+            hrsPerWeek : 7,
+            cardio : 'yes',
+            swimmingPool : 'no',
+            snacks : 'no',
+            vipRestroom : 'no',
+            Aerobics : 'yes',
+            crossfit : 'yes',
+        },
+        premium_plan : {
+            shortDesc : 'Make a list of your achievements toward your long-term',
+            price : 250 , 
+            duration : 3,
+            hrsPerWeek : 12,
+            cardio : 'yes',
+            swimmingPool : 'no',
+            snacks : 'no',
+            vipRestroom : 'yes',
+            Aerobics : 'yes',
+            crossfit : 'yes',
+        },
+        premium_plan : {
+            shortDesc : 'Use a past defeat as a motivator. Remind yourself you.',
+            price : 350 , 
+            duration : 4,
+            hrsPerWeek : 16,
+            cardio : 'yes',
+            swimmingPool : 'yes',
+            snacks : 'no',
+            vipRestroom : 'yes',
+            Aerobics : 'yes',
+            crossfit : 'yes',
+        },
+    }
+]
